bad95f99149fb676a5826de676295df12c511523
ChampionVikash <4355vikashkumar4355@gmail.com>
2023-01-16 22:41:40 +0530

    Add files via upload

---
+++ move search/js/app.js
@@ -0,0 +1,60 @@
+        // most popular movies
+const APIURL = 
+"https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+  
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";  
+      //   searched movies
+const SEARCHAPI =
+"https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+
+const movieBox = document.querySelector("#movie-box")
+const getMovies = async(api) => {
+    const response = await fetch(api)
+    const data = await response.json()
+   //  console.log(data)
+   showMovies(data.results)
+}
+const showMovies = (data)=> {
+   //EMPTY THE MOVIE BOX
+   movieBox.innerHTML = "";
+   // console.log(data)
+   data.forEach(
+      (item) => {
+         //  console.log(item)
+         const box= document.createElement("div")
+         box.classList.add("box")
+         box.innerHTML =`
+          <img src="${IMGPATH + item.poster_path}" alt="" />
+          <div class="overlay">
+             <div class="title">
+               <h2>${item.original_title}</h2>
+               <span>${item.vote_average}<span>
+            </div>
+               <h3>Overview:</h3>
+             <p>
+              ${item.overview}
+             </p>
+          </div>
+         ` ;
+         movieBox.appendChild(box)
+       }
+   )
+}
+
+document.querySelector("#search").addEventListener(
+   "keyup",
+   function(event){
+      console.log(event.target.value)
+      if(event.target.value != ""){
+            // search movies
+         getMovies(SEARCHAPI + event.target.value)
+      }else{
+         //popular
+         getMovies(APIURL)
+      }
+
+   }
+)
+//   init call
+getMovies(APIURL);
